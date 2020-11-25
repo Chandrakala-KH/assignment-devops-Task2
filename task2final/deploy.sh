1383d604ef0b0fe895440ba1cb5ce19e89a9557c
chashakala <42860183+chashakala@users.noreply.github.com>
2020-11-25 12:25:42 +1100

    Update deploy.sh

--- task2final/deploy.sh
+++ task2final/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-kubectl apply -f network-policy.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f secret.yaml
+
+  kubectl apply -f secret.yaml --validate=false --namespace=default
+
+  kubectl apply -f Network_policy.yaml --validate=false
+
+  kubectl apply -f deployment.yaml --validate=false --namespace=default
